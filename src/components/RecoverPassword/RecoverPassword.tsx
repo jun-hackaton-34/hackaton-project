7351b3b7892693fee173922963634e3ee785d9f5
Andrey Loginov <andrey12q112@gmail.com>
2023-11-12 06:08:00 +0300

    improve ui, remove input after submitting

--- src/components/RecoverPassword/RecoverPassword.tsx
+++ src/components/RecoverPassword/RecoverPassword.tsx
@@ -7,9 +7,10 @@ import Button from '../Button';
 export function RecoverPassword() {
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
   const { passwordReset }: any = useAuth();
-  const [email, setEmail] = useState('');
-  const [loading, setLoading] = useState(false);
-  const [msg, setMsg] = useState('');
+  const [email, setEmail] = useState<string>('');
+  const [success, setSuccess] = useState<boolean>(false);
+  const [loading, setLoading] = useState<boolean>(false);
+  const [msg, setMsg] = useState<string>('');
 
   const handleRecover = async () => {
     try {
@@ -18,6 +19,7 @@ export function RecoverPassword() {
       if (email) {
         await passwordReset(email);
         setMsg('Ссылка на восстановление пароля отправлена на Вашу почту!');
+        setSuccess(true);
       } else {
         setMsg('Пожалуйста, введите действительный email-адрес');
       }
@@ -30,20 +32,26 @@ export function RecoverPassword() {
   return (
     <>
       <h2>Восстановление доступа</h2>
-      <label htmlFor="email">
-        Эл. почта:
-        <input
-          type="email"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-        />
-      </label>
-      <Button onClick={handleRecover} disabled={loading}>
-        {loading ? 'Загрузка' : 'Отправить запрос'}
-      </Button>
-      <p>
-        Попробовать <Link to="/login">войти</Link> снова?
-      </p>
+      {success && !loading ? (
+        <div>Следуйте инструкциям в письме</div>
+      ) : (
+        <>
+          <label htmlFor="email">
+            Эл. почта:
+            <input
+              type="email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+          </label>
+          <Button onClick={handleRecover} disabled={loading}>
+            {loading ? 'Загрузка' : 'Отправить запрос'}
+          </Button>
+          <p>
+            Попробовать <Link to="/login">войти</Link> снова?
+          </p>
+        </>
+      )}
       {msg && <div>{msg}</div>}
     </>
   );
