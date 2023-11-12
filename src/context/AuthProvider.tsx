619c7f1602ae4b455d76578094ff37c7a319ee30
Andrey Loginov <andrey12q112@gmail.com>
2023-11-12 05:12:01 +0300

    add authentication

---
+++ src/context/AuthProvider.tsx
@@ -0,0 +1,86 @@
+import {
+  createContext,
+  ReactNode,
+  useContext,
+  useEffect,
+  useState,
+} from 'react';
+import { User } from '@supabase/supabase-js';
+
+import { supabase } from '../lib/supabase';
+import { IAuthContext } from '../resources/AuthContext.interface';
+
+const AuthContext = createContext<IAuthContext | null>({
+  auth: false,
+  user: {} as User,
+});
+
+// eslint-disable-next-line react-refresh/only-export-components
+export const useAuth = () => useContext(AuthContext);
+
+const login = (email: string, password: string) =>
+  supabase.auth.signInWithPassword({ email, password });
+
+const signOut = () => supabase.auth.signOut();
+
+const passwordReset = (email: string) =>
+  supabase.auth.resetPasswordForEmail(email, {
+    redirectTo: 'http://localhost:5173/updatepassword',
+  });
+
+const updatePassword = (updatedPassword: string) =>
+  supabase.auth.updateUser({ password: updatedPassword });
+
+interface AuthProviderProps {
+  children: ReactNode;
+}
+
+function AuthProvider({ children }: AuthProviderProps) {
+  const [auth, setAuth] = useState<boolean>(false);
+  const [user, setUser] = useState<User | null>(null);
+
+  const [loading, setLoading] = useState<boolean>(false);
+
+  useEffect(() => {
+    setLoading(true);
+    const getUser = async () => {
+      const { data } = await supabase.auth.getUser();
+      const { user: currentUser } = data;
+      if (currentUser) {
+        setUser(currentUser ?? null);
+        setAuth(true);
+      }
+
+      setLoading(false);
+    };
+    getUser();
+
+    const { data } = supabase.auth.onAuthStateChange(async (event, session) => {
+      if (event === 'PASSWORD_RECOVERY') {
+        setAuth(false);
+      } else if (event === 'SIGNED_IN') {
+        if (session) {
+          setUser(session.user);
+          setAuth(true);
+        }
+      } else if (event === 'SIGNED_OUT') {
+        setAuth(false);
+        setUser(null);
+      }
+    });
+    return () => {
+      data.subscription.unsubscribe();
+    };
+  }, []);
+
+  return (
+    <AuthContext.Provider
+      // eslint-disable-next-line react/jsx-no-constructed-context-values
+      value={{ auth, user, login, signOut, passwordReset, updatePassword }}
+    >
+      {loading ? 'Loading' : children}
+    </AuthContext.Provider>
+  );
+}
+
+export default AuthProvider;
